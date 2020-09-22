45bc686bd4bcdabe315244d2126a493daecde11b
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-09-22 05:22:46 -0700

    add react version and get a basic grid working

---
+++ reactversion/react-version/src/Components/Square.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+export class Square extends React.Component {
+  render() {
+    return <button className="square">{/* {TODO} */}</button>;
+  }
+}
